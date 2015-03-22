65f017adc41c458b608d581e2b5f89a884632a1e
Vamshideep <vamshideep@gmail.com>
2015-03-22 14:57:19 -0700

    First Commit

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# example of the run script for running the word count
+
+# first I'll load all my dependencies
+
+# next I'll make sure that all my programs (written in Python in this example) have the proper permissions
+#chmod a+x my_word_count.py
+#chmod a+x my_running_median.py
+
+# finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
+python ./src/wordCount.py
+
---
+++ src/wordCount.py
@@ -0,0 +1,89 @@
+import string
+import operator
+import pandas as pd
+import os
+
+path = './downloads/Courseera/insight/python/'
+
+#Input directory
+sub_dir = 'wc_input/'
+file_location = path + sub_dir
+
+#Output directory
+op_dir = 'wc_output/'
+op_location = path + op_dir
+
+#Output Files
+wc_result = op_location+'wc_result.txt'
+med_result = op_location+'med_result.txt'
+
+#Initializing the arrays
+wordCountPerLine = []
+medianCount = []
+words={}
+totWords ={}
+totMedian = []
+
+#List of punctuations to ignore
+punc=set(",./;'?&\'\"-[]{}|_+=()!@#$%^&*0123456789")
+
+def median(lst):
+    sortedLst = sorted(lst)
+    lstLen = len(lst)
+    index = (lstLen - 1) // 2
+
+    if (lstLen % 2):
+        return float(sortedLst[index])
+    else:
+        return (sortedLst[index] + sortedLst[index + 1])/2.0
+
+def median_count(text_data, wordFreq, punc):
+    '''
+    text_data - list of strings
+    wordFreq  - list with frequent words to exclude
+    punc      - string with punctuation to exclude
+    '''
+    #words={} 
+    #medianCount = []
+    
+    for line in text_data:
+        splited = line.split()
+        lineCount = 0
+        for ww in splited:
+            lineCount = lineCount+1
+            word = ww.lower()
+            word = ''.join((x for x in word if x not in punc))
+        
+            popWords= word in wordFreq
+            if popWords == False:
+                if words.has_key(word):
+                    words[word]=words[word]+1
+                else:
+                    words[word]=1
+        wordCountPerLine.append(lineCount)
+        medianCount.append(median(wordCountPerLine))
+        #totWords= dict(totWords.items()+words.items())
+        #totMedian.extend(medianCount)    
+    return words,medianCount
+
+#For each file in the directory, calculate word count and median count
+for filename in os.listdir(file_location):
+    print filename
+    fw1 = open(file_location+filename)
+    text_data=fw1.readlines()
+    k,l = median_count(text_data,"",punc)
+
+#print k
+#print l
+
+def writeDict1(dict, filename):
+    with open(filename, "a") as f:
+        for key in sorted(dict):            
+            f.write('%-15s %s\n' % (key,dict[key]))
+
+#Write wordcount dictionary to a file named wc_result.txt
+writeDict1(k,wc_result)
+
+#Write median count list to a file named med_result.txt
+with open(med_result, "a") as f:
+    f.writelines( "%s\n" % item for item in l )
