fdc2fc427ba1aba0b36a4c66444e645a4dbac65b
Vamshideep <vamshideep@gmail.com>
2015-03-22 16:18:55 -0700

    Fixed comments

--- src/wordCount.py
+++ src/wordCount.py
@@ -1,5 +1,6 @@
 import os
 
+#Current folder in linux, if you run it in Windows, please change the below path to current folder
 path = './'
 
 #Input directory
@@ -25,6 +26,10 @@ totMedian = []
 punc=set(",./;'?&\'\"-[]{}|_+=()!@#$%^&*0123456789")
 
 def median(lst):
+    '''
+    lst - list of numbers
+    Returns median of the list
+    '''
     sortedLst = sorted(lst)
     lstLen = len(lst)
     index = (lstLen - 1) // 2
@@ -39,10 +44,8 @@ def median_count(text_data, wordFreq, punc):
     text_data - list of strings
     wordFreq  - list with frequent words to exclude
     punc      - string with punctuation to exclude
+    Returns word count and median count
     '''
-    #words={} 
-    #medianCount = []
-    
     for line in text_data:
         splited = line.split()
         lineCount = 0
@@ -59,24 +62,27 @@ def median_count(text_data, wordFreq, punc):
                     words[word]=1
         wordCountPerLine.append(lineCount)
         medianCount.append(median(wordCountPerLine))
-        #totWords= dict(totWords.items()+words.items())
-        #totMedian.extend(medianCount)    
     return words,medianCount
 
-#For each file in the directory, calculate word count and median count
-for filename in os.listdir(file_location):
-    #print filename
-    fw1 = open(file_location+filename)
-    text_data=fw1.readlines()
-    k,l = median_count(text_data,"",punc)
-
-#print k
-#print l
-
 def writeDict1(dict, filename):
+    '''
+    dict - dictionary of word count
+    filename  - writes the dictionary to this file
+    Writes word count to the file
+    '''
     with open(filename, "a") as f:
         for key in sorted(dict):            
             f.write('%-15s %s\n' % (key,dict[key]))
+
+
+#For each file in the directory, calculate word count and median count
+for filename in os.listdir(file_location):
+    fw1 = open(file_location+filename)
+    text_data=fw1.readlines()
+    #k is the word count
+    #l is the median count
+    k,l = median_count(text_data,"",punc)
+
 
 #Write wordcount dictionary to a file named wc_result.txt
 writeDict1(k,wc_result)
